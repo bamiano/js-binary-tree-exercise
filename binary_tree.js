babe42bd490c8320dd39caae64c27a8edb345117
bamiano <bamiano@email.arizona.edu>
2014-12-05 09:45:10 -0800

    Thurs night HW

--- binary_tree.js
+++ binary_tree.js
@@ -5,14 +5,58 @@ function BinTree(value){
 }
 
 
-BinTree.prototype.insert = function(){
-
+BinTree.prototype.insert = function (value){
+    if (this.value > value){
+      if (this.left === null){
+        this.left = new BinTree(value);
+      }
+      else {
+        this.left.insert(value);
+      }
+    }
+    else {
+      if (this.right === null){
+        this.right = new BinTree(value);
+      }
+      else{
+        this.right.insert(value);
+      }
+    }
+    return this;
 };
-BinTree.prototype.isLeaf = function(){
 
+BinTree.prototype.isLeaf = function () {
+    if (this.left === null && this.right === null){
+      return  true;
+    }
+    else {
+      return false;
+    }
 };
-BinTree.prototype.search = function(){
 
+
+BinTree.prototype.search = function (value){
+  if (this.value === value){
+    return true;
+  }
+
+  else if (this.value > value) {
+    if (this.left === null) {
+      return false;
+    }
+    else {
+      return this.left.search(value);
+      }
+    }
+
+  else if (this.value < value) {
+    if (this.right === null) {
+      return false;
+      }
+    else {
+      return this.right.search(value);
+      }
+    }
 };
 
 
